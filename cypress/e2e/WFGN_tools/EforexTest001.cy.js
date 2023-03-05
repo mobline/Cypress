2b9fea98fb6373a9579e5f54f16c89afecdb7ee2
Mohamed Traore <mohamed.traore@webbfontaine.com>
2023-03-05 21:32:58 +0000

    added cypress.env.json

--- cypress/e2e/WFGN_tools/EforexTest001.cy.js
+++ cypress/e2e/WFGN_tools/EforexTest001.cy.js
@@ -7,8 +7,8 @@ describe('CAS DE TEST EFOREX VERSION 3.2.0', () => {
     cy.visit('https://uat.guceg.gov.gn/tvf/tvfGen/list', { timeout: 6000 })
     //cy.get('.gLFyf').type("ANDREW THE MAIN MAN {enter}")
 
-    cy.get('#username').type('mohamed.traore@webbfontaine.com')
-    cy.get('#password').type('Abce1234!@{enter}')
+    cy.get('#username').type(Cypress.env('username'))
+    cy.get('#password').type(Cypress.env('password'))
 
 
 
