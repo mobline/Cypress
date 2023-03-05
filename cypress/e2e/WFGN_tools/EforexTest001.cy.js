6dc9265e22831d9305220d134a7645482a1b543f
Mohamed Traore <mohamed.traore@webbfontaine.com>
2023-03-05 21:22:23 +0000

    remove error

--- cypress/e2e/WFGN_tools/EforexTest001.cy.js
+++ cypress/e2e/WFGN_tools/EforexTest001.cy.js
@@ -5,7 +5,7 @@ describe('CAS DE TEST EFOREX VERSION 3.2.0', () => {
   it('passes', () => {
 
     cy.visit('https://uat.guceg.gov.gn/tvf/tvfGen/list', { timeout: 6000 })
-    //cy.get('.gLFyf').type("ANDREW BASTARD{enter}")
+    //cy.get('.gLFyf').type("ANDREW THE MAIN MAN {enter}")
 
     cy.get('#username').type('mohamed.traore@webbfontaine.com')
     cy.get('#password').type('Abce1234!@{enter}')
@@ -15,4 +15,4 @@ describe('CAS DE TEST EFOREX VERSION 3.2.0', () => {
 
 
   })
-})git remote add origin https://github.com/mobline/Cypress.gitgit remote add origin https://github.com/mobline/Cypress.gits
+})
